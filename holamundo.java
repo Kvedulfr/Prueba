fcb8eb4d20df7989dda672b5042aa3bbe18b181a
Rafael Delgado Cubilla <rafael.delgado.cubilla@iesjandula.es>
2018-01-31 14:22:00 +0100

    vacía primera linea de las tres

--- holamundo.java
+++ holamundo.java
@@ -2,7 +2,7 @@ public class HolaMundo {
  
 	public static void main(String[] args) {		
 
-		System.out.println("Hola Mundo Andújar en ramanueva");
+		System.out.println("");
 		System.out.println("Hola Mundo Andújar en marmolejo");
 
 		System.out.println("Hola Mundo Pedro Abad");
